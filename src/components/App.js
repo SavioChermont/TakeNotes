9e9098ac0918f643a3ae366fff65d64ff3884fe0
SavioChermont <saviochwt@gmail.com>
2020-10-30 23:14:15 -0300

    Ajusta App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,51 @@
 import React from "react";
 import Form from "./Form";
+import ShowNotes from "./ShowNotes";
+import "../css/App.css";
 
 class App extends React.Component {
-  state = {
-    titulo: "",
-    conteudo: "",
+  state = { notas: [] };
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  formSubmit = (data) => {
+    const nota = { titulo: data.titulo, conteudo: data.conteudo };
+    const notas = [...this.state.notas, nota];
+    this.setState({ notas: notas });
   };
 
-  formSubmit = (nota) => {
-    console.log(nota);
+  showPreview = (campo, letra) => {
+    if (campo == "titulo") {
+      document.querySelector(".preview-titulo").innerHTML = letra;
+    } else {
+      document.querySelector(".preview-conteudo").innerHTML = letra;
+    }
   };
 
   render() {
     return (
       <div className="take-note-container">
         <div className="adicionar-nota">
-          <div className="btn-new-nota">
-            <button className="btn">
-              <i className="btn-icon"> + </i>
-            </button>
-          </div>
-          <div className="formulario">
-            <Form onFormSubmit={this.formSubmit} />
+          <div className="create-note">
+            <div className="formulario">
+              <Form
+                onFormSubmit={this.formSubmit}
+                showPreview={this.showPreview}
+              />
+            </div>
+            <div className="preview-notes">
+              <div className="preview-container">
+                <h3 className="preview-titulo"> TITULO </h3>
+                <p className="preview-conteudo"> CONTEUDO </p>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="show-notes"></div>
+        <div className="show-notes">
+          <ShowNotes notas={this.state.notas} />
+        </div>
       </div>
     );
   }
